d2f4c6823b1c221b232a5956f822855e2f1c30fb
choco <choco@rambler-co.ru>
2018-07-01 11:57:35 +0300

    add first simple component

---
+++ lewagon_app/airbnb-clone/src/components/hello.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+class Hello extends React.Component {
+    render() {
+        // JSX
+        return <div>Hello from hello.js!</div>;
+    }
+}
+
+
+export default Hello;
--- lewagon_app/airbnb-clone/src/index.js
+++ lewagon_app/airbnb-clone/src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+//import App from './App';
+//import registerServiceWorker from './registerServiceWorker';
+import Hello from './components/hello';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+//ReactDOM.render(<App />, document.getElementById('root'));
+//registerServiceWorker();
+
+ReactDOM.render(<Hello />, document.getElementById('root'));
