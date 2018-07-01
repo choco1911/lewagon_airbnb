856b0b926dd0392856babda26f7735450a1cfd2e
choco <choco@rambler-co.ru>
2018-07-01 23:03:31 +0300

    create flat component

--- lewagon_app/airbnb-clone/src/index.js
+++ lewagon_app/airbnb-clone/src/index.js
@@ -1,20 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-//import App from './App';
+import App from './App';
 //import registerServiceWorker from './registerServiceWorker';
-import Hello from './components/hello';
+//import Hello from './components/hello';
 
 //ReactDOM.render(<App />, document.getElementById('root'));
 //registerServiceWorker();
 
 
-const myprop =  <Hello firstname="Boris" lastname="Paillard"/>;
+// const myprop =  <Hello firstname="Boris" lastname="Paillard"/>;
 
-const myprop2 = <div>
-    <Hello firstname="Boris" lastname="Paillard"/>
-    <Hello firstname="Romain" lastname="Paillard"/>
- </div>;
+//const myprop2 = <div>
+ //   <Hello firstname="Boris" lastname="Paillard"/>
+  //  <Hello firstname="Romain" lastname="Paillard"/>
+// </div>;
 
 // ReactDOM.render(<Hello firstname="Boris" lastname="Paillard"/>, document.getElementById('root'));
-ReactDOM.render(myprop2, document.getElementById('root'));
+//ReactDOM.render(myprop2, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
