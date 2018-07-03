85250a48e9538138cab3991623eec4fecacdc23e
choco <choco@rambler-co.ru>
2018-07-03 15:34:26 +0300

    using map func for dymanic flat list

--- lewagon_app/airbnb-clone/src/App.js
+++ lewagon_app/airbnb-clone/src/App.js
@@ -15,15 +15,21 @@ class App extends Component {
         "lng": 2.346890
     };
 
+    const flats = [ flat, flat, flat, flat];
+
     return (
         <div className="app">
             <div className="main">
                 <div className="search">
                 </div>
                 <div className="flats">
-                    <Flat flat={flat} />
-                    <Flat flat={flat} />
-                    <Flat flat={flat} />
+                /*
+                    {flats.map(function(flat) {
+                            return <Flat flat={flat} />
+                    })}
+                */
+                // ES6 style
+                    {flats.map(flat =>  <Flat flat={flat} />)}
                 </div>
             </div>
             <div className="map">
