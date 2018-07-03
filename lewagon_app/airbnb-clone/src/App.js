560798a87c973abba16e4532c8eefcda1e835b55
choco <choco@rambler-co.ru>
2018-07-03 20:07:25 +0300

    1. implement state 2. get json data over http from github using ajax 3. create dymanic list from json

--- lewagon_app/airbnb-clone/src/App.js
+++ lewagon_app/airbnb-clone/src/App.js
@@ -4,32 +4,35 @@ import './App.css';
 import Flat from './components/flat';
 
 class App extends Component {
+  constructor(props) {
+        super(props);
+        this.state = {
+                flats: []
+        };
+  }
+
+ componentDidMount() {
+    const url = "https://raw.githubusercontent.com/lewagon/flats-boilerplate/master/flats.json"
+    fetch(url) //AJAX
+        .then(response => response.json())
+        .then(data => {
+            /*
+            console.log(data);
+            */
+            this.setState({
+                flats: data
+            });
+        });
+ }
+
   render() {
-    const flat = {
-        "id": 145,
-        "name": "Charm at the Steps of the Sacre Coeur/Montmartre",
-        "imageUrl": "https://raw.githubusercontent.com/lewagon/flats-boilerplate/master/images/flat1.jpg",
-        "price": 164,
-        "priceCurrency": "EUR",
-        "lat": 48.884211,
-        "lng": 2.346890
-    };
-
-    const flats = [ flat, flat, flat, flat];
-
     return (
         <div className="app">
             <div className="main">
                 <div className="search">
                 </div>
                 <div className="flats">
-                /*
-                    {flats.map(function(flat) {
-                            return <Flat flat={flat} />
-                    })}
-                */
-                // ES6 style
-                    {flats.map(flat =>  <Flat flat={flat} />)}
+                    {this.state.flats.map(flat =>  <Flat flat={flat} />)}
                 </div>
             </div>
             <div className="map">
