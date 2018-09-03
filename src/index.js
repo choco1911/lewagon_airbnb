312e50d33291abbe753782e885e475f6181c20f0
choco <choco@rambler-co.ru>
2018-09-04 00:13:56 +0300

    REAL Start template

--- src/index.js
+++ src/index.js
@@ -1,11 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-//import App from './App';
-//import registerServiceWorker from './registerServiceWorker';
-import Hello from './components/hello';
-
-//ReactDOM.render(<App />, document.getElementById('root'));
-//registerServiceWorker();
+import Hello from './hello';
 
 ReactDOM.render(<Hello />, document.getElementById('root'));
